84b2864555f883614c60298e2a0e929fc46b9c4e
Josip Vulic <jovulic@gmail.com>
2025-03-28 12:30:48 -0400

    ref(neovim): cleanup neovim installs to match config

--- system/neovim.nix
+++ system/neovim.nix
@@ -33,56 +33,80 @@ with lib;
       pkgs.gdu # astrovim
       pkgs.bottom # astrovim
       pkgs.clang # astrovim
-      pkgs.nodePackages.bash-language-server # pack(bash)
-      pkgs.shfmt # pack(bash)
-      pkgs.shellcheck # pack(bash)
-      pkgs.dockerfile-language-server-nodejs # pack(dockerfile)
-      pkgs.hadolint # pack(dockerfile)
-      pkgs.gopls # pack(go)
-      pkgs.gofumpt # pack(go)
-      pkgs.gotools # pack(go) "goimports"
-      pkgs.gomodifytags # pack(go)
-      pkgs.iferr # pack(go)
-      pkgs.impl # pack(go)
-      pkgs.delve
+
+      # astrovim pack=lua
+      pkgs.lua-language-server
+      pkgs.selene
+      pkgs.stylua
+
+      # astrovim pack=bash
+      pkgs.shfmt
+      pkgs.shellcheck
+      pkgs.bash-language-server
+
+      # astrovim pack=docker
+      # docker_compose_language_service
+      # dockerls
+      pkgs.hadolint
+      pkgs.prettierd
+
+      # astrovim pack=fish
+
+      # astrovim pack=json
+      # jsonls
+
+      # astrovim pack=yaml
+      # yamlls
+      pkgs.prettierd
+
+      # astrovim pack=toml
+      pkgs.taplo
+
+      # astrovim pack=markdown
+      pkgs.marksman
+      pkgs.prettierd
+
+      # astrovim pack=sql
+      pkgs.sqls
+      pkgs.go # required by sqls
+      pkgs.sqlfluff
+
+      # astrovim pack=proto
+      pkgs.buf
+      # buf_ls
+
+      # astrovim pack=nix
+      pkgs.nixd
+      pkgs.statix
+      pkgs.deadnix
+
+      # astrovim pack=go
+      pkgs.gopls
+      pkgs.gotools # goimports
+      pkgs.gomodifytags
       pkgs.gotests
-      pkgs.nodePackages.vscode-json-languageserver # pack(json)
-      (pkgs.writeShellScriptBin "vscode-json-language-server" ''
-        vscode-json-languageserver $@
-      '') # because of the jsonls command configuration
-      pkgs.lua-language-server # pack(lua)
-      pkgs.stylua # pack(lua)
-      pkgs.selene # pack(lua)
-      pkgs.marksman # pack(markdown)
-      pkgs.alejandra # pack(nix)
-      pkgs.deadnix # pack(nix)
-      pkgs.statix # pack(nix)
-      pkgs.buf # pack(proto)
-      pkgs.pyright # pack(python)
-      pkgs.black # pack(python)
-      pkgs.isort # pack(python)
-      pkgs.python311Packages.debugpy # pack(python)
-      pkgs.sqls # pack(sql)
-      pkgs.taplo # pack(toml)
-      (pkgs.symlinkJoin {
-        name = "typescript-language-server";
-        paths = [ pkgs.nodePackages.typescript-language-server ];
-        buildInputs = [ pkgs.makeWrapper ];
-        postBuild = ''
-          wrapProgram $out/bin/typescript-language-server \
-            # NB(jv): No longer require/working (error of "unknown option").
-            # --add-flags --tsserver-path=${pkgs.nodePackages.typescript}/lib/node_modules/typescript/lib/
-        '';
-      }) # pack(typescript)
-      pkgs.nodePackages.eslint # pack(typescript) or "vscode-extensions.dbaeumer.vscode-eslint"?
-      pkgs.prettierd # pack(typescript,yaml,markdown)
-      pkgs.yaml-language-server # pack(yaml)
-      pkgs.nixd # pack(nix)
-      pkgs.nixfmt-rfc-style # custom~pack(nix)
-      pkgs.nodePackages.vls # pack(vue)
+      pkgs.iferr
+      pkgs.impl
+
+      # astrovim pack=typescript
+      pkgs.vtsls
+      pkgs.eslint
+      pkgs.prettierd
+
+      # astrovim pack=vue
+      pkgs.vue-language-server
+
+      # astrovim pack=python
+      pkgs.basedpyright
+      pkgs.black
+      pkgs.isort
+
+      # astrovim pack=cpp
+      pkgs.clang-tools # clangd, clang-format
+
       # markdown-preview (cd ~/.local/share/nvim/lazy/markdown-preview.nvim; npm install)
+
       pkgs.neovide
-      pkgs.clang-tools # pack(cpp)
     ];
 
     environment.variables = {
