0b9c79aa24f6ba1e62109df67caabbb9cb22f8a6
Josip Vulic <jovulic@gmail.com>
2025-10-25 11:50:44 -0400

    upd(gemini): update gemini to use binary and override to update version

--- modules/system/gemini.nix
+++ modules/system/gemini.nix
@@ -20,7 +20,13 @@ with lib;
   };
   config = mkIf cfg.enable {
     environment.systemPackages = [
-      unstablepkgs.gemini-cli
+      (unstablepkgs.gemini-cli-bin.overrideAttrs rec {
+        version = "0.10.0";
+        src = unstablepkgs.fetchurl {
+          url = "https://github.com/google-gemini/gemini-cli/releases/download/v${version}/gemini.js";
+          hash = "sha256-jwyx5HWjPi2S5GQFxV+VeuwrmjmLi+F1nzw4YMfNSiA=";
+        };
+      })
     ];
   };
 }
