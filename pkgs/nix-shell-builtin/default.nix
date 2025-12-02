65b9aa5012eb9d62040a54591748b52d70eed0b9
Josip Vulic <jovulic@gmail.com>
2025-12-02 15:33:56 -0500

    ftr(pkgs): update nix-shell-builtin to v2.1.1

--- pkgs/nix-shell-builtin/default.nix
+++ pkgs/nix-shell-builtin/default.nix
@@ -4,7 +4,7 @@ pkgs.callPackage
     pkgs.fetchFromGitHub {
       owner = "jovulic";
       repo = "nix-shell-builtin";
-      rev = "v2.0.0";
+      rev = "v2.1.1";
       hash = "sha256-YG9fKpQWBTNHxiqRaDi3di/gIkGleOlAA6kqKyrCRwY=";
     }
     + "/plugin.nix"
