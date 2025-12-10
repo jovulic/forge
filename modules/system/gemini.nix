8fc7d5da819582029e19111f3604cbb8811a2754
Josip Vulic <jovulic@gmail.com>
2025-12-10 18:51:22 -0500

    add(gemini): update gemini-cli to 0.20.0

--- modules/system/gemini.nix
+++ modules/system/gemini.nix
@@ -20,7 +20,26 @@ with lib;
   };
   config = mkIf cfg.enable {
     environment.systemPackages = [
-      unstablepkgs.gemini-cli
+      (unstablepkgs.gemini-cli.overrideAttrs (
+        final: prev:
+        let
+          version = "0.20.0";
+          src = prev.src.override {
+            tag = "v${final.version}";
+            hash = "sha256-6+fT9/npYrngAPeAP7pA6DYNuCVWm1lKpSVP4Ux4ddw=";
+          };
+          npmDeps = prev.npmDeps.overrideAttrs (_: {
+            inherit version;
+            inherit src;
+            outputHash = "sha256-wbr/9IitwQxBVFskCyGZfWy6FmIGZAVYLbF/sMJ2X+s=";
+          });
+        in
+        {
+          inherit version;
+          inherit src;
+          inherit npmDeps;
+        }
+      ))
     ];
   };
 }
