92234bec8a90f208b92161479068b9e10f7f0d4c
Josip Vulic <jovulic@gmail.com>
2025-12-18 14:00:08 -0500

    del(kubernetes): remove home config until we bring it back online

--- modules/home/kubernetes.nix
+++ modules/home/kubernetes.nix
@@ -31,48 +31,49 @@ with lib;
       '';
     };
 
-    home.file.".kube/config-home.yaml" = {
-      text = ''
-        apiVersion: v1
-        clusters:
-        - cluster:
-            certificate-authority-data: ${builtins.shell "gopass show -o --nosync home/k8s/-/ca-certificate"}
-            server: https://frost.lan:6443
-          name: home
-        contexts:
-        - context:
-            cluster: home
-            user: me
-          name: me@home
-        - context:
-            cluster: home
-            user: me
-          name: me-oidc@home
-        current-context: ""
-        kind: Config
-        preferences: {}
-        users:
-        - name: me
-          user:
-            client-certificate-data: ${builtins.shell "gopass show -o --nosync home/k8s/-/me-certificate"}
-            client-key-data: ${builtins.shell "gopass show -o --nosync home/k8s/-/me-certificate-key"}
-        - name: me-oidc
-          user:
-            exec:
-              apiVersion: client.authentication.k8s.io/v1beta1
-              command: kubectl
-              args:
-              - oidc-login
-              - get-token
-              - --oidc-issuer-url=https://sso.bootstrap.home/dex
-              - --oidc-client-id=kubernetes
-              - --oidc-client-secret=${builtins.shell "gopass show -o --nosync home/bm/optiplexm/portunus/client/kubernetes"}
-              - --oidc-extra-scope=email
-      '';
-    };
+    # home.file.".kube/config-home.yaml" = {
+    #   text = ''
+    #     apiVersion: v1
+    #     clusters:
+    #     - cluster:
+    #         certificate-authority-data: ${builtins.shell "gopass show -o --nosync home/k8s/-/ca-certificate"}
+    #         server: https://frost.lan:6443
+    #       name: home
+    #     contexts:
+    #     - context:
+    #         cluster: home
+    #         user: me
+    #       name: me@home
+    #     - context:
+    #         cluster: home
+    #         user: me
+    #       name: me-oidc@home
+    #     current-context: ""
+    #     kind: Config
+    #     preferences: {}
+    #     users:
+    #     - name: me
+    #       user:
+    #         client-certificate-data: ${builtins.shell "gopass show -o --nosync home/k8s/-/me-certificate"}
+    #         client-key-data: ${builtins.shell "gopass show -o --nosync home/k8s/-/me-certificate-key"}
+    #     - name: me-oidc
+    #       user:
+    #         exec:
+    #           apiVersion: client.authentication.k8s.io/v1beta1
+    #           command: kubectl
+    #           args:
+    #           - oidc-login
+    #           - get-token
+    #           - --oidc-issuer-url=https://sso.bootstrap.home/dex
+    #           - --oidc-client-id=kubernetes
+    #           - --oidc-client-secret=${builtins.shell "gopass show -o --nosync home/bm/optiplexm/portunus/client/kubernetes"}
+    #           - --oidc-extra-scope=email
+    #   '';
+    # };
 
     home.sessionVariables = {
-      KUBECONFIG = "$HOME/.kube/config:$HOME/.kube/config-home.yaml";
+      KUBECONFIG = "$HOME/.kube/config";
+      # KUBECONFIG = "$HOME/.kube/config:$HOME/.kube/config-home.yaml";
       KUBETAIL_SHOW_COLOR_INDEX = "true";
       KUBETAIL_SKIP_COLORS = "16,17,18,19,20,21";
     };
