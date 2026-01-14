38ef9fcb2477a09e2cea37fc4d6f5f7c4a855f95
Josip Vulic <jovulic@gmail.com>
2026-01-14 15:51:49 -0500

    ref(steam): remove bwrap patch going instead with gamemode for caps

--- modules/system/steam/default.nix
+++ modules/system/steam/default.nix
@@ -19,85 +19,53 @@ with lib;
       };
     };
   };
-  config =
-    # We give steam a patched brwap that allows applications then to
-    # access high priority contexts.
-    # https://wiki.nixos.org/wiki/VR#SteamVR
-    #
-    # Additionally, we may need to replace Steam's own bwrap binary with
-    # a symbolic link to this modified bwrap binary, found at
-    # ~/.local/share/Steam/ubuntu12_32/steam-runtime/usr/libexec/steam-runtime-tools-0/srt-bwrap.
-    # Steam will periodically replace this modification with its own binary
-    # when steam-runtime updates, so you may need to re-apply this change if it
-    # breaks.
-    #
-    # NOTE: It seems we need to make this bwrap generally available in order
-    # for things to wor (add it to system packages). Odd, but going to just do
-    # that for now.
-    let
-      patchedBwrap = pkgs.bubblewrap.overrideAttrs (o: {
-        patches = (o.patches or [ ]) ++ [
-          ../../../patches/bwrap.patch
-        ];
-      });
-    in
-    mkIf cfg.enable {
-      programs.steam = {
-        enable = true;
-        package = pkgs.steam.override {
-          buildFHSEnv = (
-            args:
-            (
-              (pkgs.buildFHSEnv.override {
-                bubblewrap = patchedBwrap;
-              })
-              (
-                args
-                // {
-                  extraBwrapArgs = (args.extraBwrapArgs or [ ]) ++ [ "--cap-add ALL" ];
-                }
-              )
-            )
-          );
-        };
-        # https://wiki.nixos.org/wiki/Steam#gamescope
-        gamescopeSession = {
-          enable = true;
-        };
-        extraCompatPackages = [
-          pkgs.proton-ge-bin
-        ];
-      };
-
-      # https://wiki.nixos.org/wiki/GameMode
-      # steam > gamemoderun %command%
-      programs.gamemode = {
-        enable = true;
-      };
-
-      environment.systemPackages = [
-        patchedBwrap
-        pkgs.protontricks # a simple wrapper for running winetricks commands for proton-enabled games
-        pkgs.protonup-qt # install and manage proton-ge for steam
-        unstablepkgs.wlx-overlay-s # Wayland/X11 desktop overlay for SteamVR and OpenXR, Vulkan edition
-        (pkgs.writeShellScriptBin "steamvr-patch" ''
-          # Iterate over all shared object files under steamvr and run patch
-          # referencing the steam FHS for libraries.
-          STOREPATH=$(nix-store -qR `which steam` | grep steam-fhs)/lib64
-          find ~/.local/share/Steam/steamapps/common/SteamVR/ -name "*.so*" | while read line ; do echo "Patching $line"; patchelf --add-rpath $STOREPATH $line ; done
-          find /home/me/.local/share/Steam/steamapps/common/SteamVR/tools/steamvr_environments/game/steamtours/bin/linuxsteamrt64 -name "*.so*" | while read line ; do echo "Patching $line"; patchelf --add-rpath /home/me/.local/share/Steam/steamapps/common/SteamVR/tools/steamvr_environments/game/bin/linuxsteamrt64/ $line ; done
-          find /home/me/.local/share/Steam/steamapps/common/SteamVR/bin/linux64/ -name "*.so*" | while read line ; do echo "Patching $line"; patchelf --add-rpath $(dirname $line) $line ; done
-          find /home/me/.local/share/Steam/steamapps/common/SteamVR/bin/linux64/ -name "*.so*" | while read line ; do echo "Patching $line"; patchelf --add-rpath /home/me/.local/share/Steam/steamapps/common/SteamVR/tools/steamvr_environments/game/bin/linuxsteamrt64/ $line ; done
-
-          # Also, update the wlx-overlay-s manifest store path. It can become
-          # an issue if the one currently referenced in garbage collected.
-          echo "Update wlx-overlay-s manifest"
-          wlx-overlay-s --replace
-        '') # script that patches a variety of files related to SteamVR.
-      ];
-
-      # NOTE: Command to iterate over all SteamVR shared object files printing out dependencies that do not exist.
-      #
-      # find ~/.local/share/Steam/steamapps/common/SteamVR/ -name "*.so*" | while read line ; do echo "Printing $line"; ldd $line | grep "not found" ; done
+  config = mkIf cfg.enable {
+    # https://wiki.nixos.org/wiki/GameMode
+    # steam > gamemoderun %command%
+    programs.gamemode = {
+      enable = true;
     };
+
+    programs.gamescope = {
+      enable = true;
+      capSysNice = true;
+    };
+
+    programs.steam = {
+      enable = true;
+
+      # https://wiki.nixos.org/wiki/Steam#gamescope
+      gamescopeSession = {
+        enable = true;
+      };
+
+      extraCompatPackages = [
+        pkgs.proton-ge-bin
+      ];
+    };
+
+    environment.systemPackages = [
+      pkgs.protontricks # a simple wrapper for running winetricks commands for proton-enabled games
+      pkgs.protonup-qt # install and manage proton-ge for steam
+      unstablepkgs.wlx-overlay-s # Wayland/X11 desktop overlay for SteamVR and OpenXR, Vulkan edition
+      (pkgs.writeShellScriptBin "steamvr-patch" ''
+        # Iterate over all shared object files under steamvr and run patch
+        # referencing the steam FHS for libraries.
+        STOREPATH=$(nix-store -qR `which steam` | grep steam-fhs)/lib64
+        find ~/.local/share/Steam/steamapps/common/SteamVR/ -name "*.so*" | while read line ; do echo "Patching $line"; patchelf --add-rpath $STOREPATH $line ; done
+        find /home/me/.local/share/Steam/steamapps/common/SteamVR/tools/steamvr_environments/game/steamtours/bin/linuxsteamrt64 -name "*.so*" | while read line ; do echo "Patching $line"; patchelf --add-rpath /home/me/.local/share/Steam/steamapps/common/SteamVR/tools/steamvr_environments/game/bin/linuxsteamrt64/ $line ; done
+        find /home/me/.local/share/Steam/steamapps/common/SteamVR/bin/linux64/ -name "*.so*" | while read line ; do echo "Patching $line"; patchelf --add-rpath $(dirname $line) $line ; done
+        find /home/me/.local/share/Steam/steamapps/common/SteamVR/bin/linux64/ -name "*.so*" | while read line ; do echo "Patching $line"; patchelf --add-rpath /home/me/.local/share/Steam/steamapps/common/SteamVR/tools/steamvr_environments/game/bin/linuxsteamrt64/ $line ; done
+
+        # Also, update the wlx-overlay-s manifest store path. It can become
+        # an issue if the one currently referenced in garbage collected.
+        echo "Update wlx-overlay-s manifest"
+        wlx-overlay-s --replace
+      '') # script that patches a variety of files related to SteamVR.
+    ];
+
+    # NOTE: Command to iterate over all SteamVR shared object files printing out dependencies that do not exist.
+    #
+    # find ~/.local/share/Steam/steamapps/common/SteamVR/ -name "*.so*" | while read line ; do echo "Printing $line"; ldd $line | grep "not found" ; done
+  };
 }
