8f101e95c0b23a6bd0a80f4ce54bf49b534f031b
Josip Vulic <jovulic@gmail.com>
2026-02-09 10:30:08 -0500

    ftr(gemini): update gemini to 0.27.3

--- modules/system/gemini.nix
+++ modules/system/gemini.nix
@@ -23,20 +23,33 @@ with lib;
       (unstablepkgs.gemini-cli.overrideAttrs (
         final:
         let
-          version = "0.21.1";
+          version = "0.27.3";
           src = final.src.override {
             tag = "v${version}";
-            hash = "sha256-WhQbEgr1NhReexVbCxOv11mcP+gftl1J7/FJVdiADXA=";
+            hash = "sha256-JUSl5yRJ2YtTCMfPv7oziaZG4yNnsucKlvtjfuzZO+I=";
           };
           npmDeps = unstablepkgs.fetchNpmDeps {
             inherit src;
-            hash = "sha256-Q6mtA+Y3k4VoazJ4+uKIFbaC/lUTQFSpXxfXoP7i6Lc=";
+            hash = "sha256-euy7QwuoJI+07KMUMcRAmmH/zyYgF9wFiLSF4OwQivo=";
           };
         in
         {
           inherit version;
           inherit src;
           inherit npmDeps;
+
+          # j
+          disallowedReferences = [ ];
+
+          postPatch = with unstablepkgs; ''
+            # Remove node-pty from manifests.
+            ${jq}/bin/jq 'del(.optionalDependencies."node-pty")' package.json > package.json.tmp && mv package.json.tmp package.json
+            ${jq}/bin/jq 'del(.optionalDependencies."node-pty")' packages/core/package.json > packages/core/package.json.tmp && mv packages/core/package.json.tmp packages/core/package.json
+
+            # Fix ripgrep path.
+            substituteInPlace packages/core/src/tools/ripGrep.ts \
+              --replace-fail "await ensureRgPath();" "'${lib.getExe ripgrep}';"
+          '';
         }
       ))
     ];
