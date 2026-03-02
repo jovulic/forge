c207d10930560c4ca1e943d545c6918484e11b1c
Josip Vulic <jovulic@gmail.com>
2026-03-02 11:29:06 -0500

    add(certificates): add k8s ca to cert bundle

--- modules/system/certificates.nix
+++ modules/system/certificates.nix
@@ -22,7 +22,10 @@ with lib;
       let
         homelab = "/etc/ssl/certs/ca-homelab.crt";
         homelabContent = builtins.shell "if [ -f ${homelab} ]; then cat ${homelab}; fi";
+        k8sHomelab = "/etc/ssl/certs/k8s-ca-homelab.crt";
+        k8sHomelabContent = builtins.shell "if [ -f ${k8sHomelab} ]; then cat ${k8sHomelab}; fi";
       in
-      optional (homelabContent != "") homelabContent;
+      (optional (homelabContent != "") homelabContent)
+      ++ (optional (k8sHomelabContent != "") k8sHomelabContent);
   };
 }
