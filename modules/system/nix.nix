c9a1978370ede9e9a7f2542541dfde912b34ae1b
Josip Vulic <jovulic@gmail.com>
2026-06-01 17:40:57 +1000

    fix(nix): correctly override nh by using unwrapped derivation

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -38,38 +38,39 @@ with lib;
     };
 
     programs.nh = {
-      # source: https://github.com/NixOS/nixpkgs/blob/nixos-25.11/pkgs/by-name/nh/nh/package.nix#L74
-      package = pkgs.nh.overrideAttrs (final: rec {
-        version = "af45145463dd47d40ea5868bd759546e4fc5adbd";
-        src = pkgs.fetchFromGitHub {
-          owner = "jovulic";
-          repo = "nh";
-          rev = version;
-          hash = "sha256-CViR0PII1XjSjCePQtuZ0Q1d1NqDv+Za1kOZtAu0R4o=";
-        };
-        patches = [ ];
-        cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
-          name = "${final.pname}-${version}";
-          inherit src;
-          hash = "sha256-V+Udw9u8PRl3fE/ZFO0zVlrtHC+vXHNAp5pt4QbFVKA=";
-        };
-        postInstall = ''
-          mkdir -p completions
+      package = pkgs.nh.override {
+        nh-unwrapped = pkgs.nh-unwrapped.overrideAttrs (final: rec {
+          version = "af45145463dd47d40ea5868bd759546e4fc5adbd";
+          src = pkgs.fetchFromGitHub {
+            owner = "jovulic";
+            repo = "nh";
+            rev = version;
+            hash = "sha256-CViR0PII1XjSjCePQtuZ0Q1d1NqDv+Za1kOZtAu0R4o=";
+          };
+          patches = [ ];
+          cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+            name = "${final.pname}-${version}";
+            inherit src;
+            hash = "sha256-V+Udw9u8PRl3fE/ZFO0zVlrtHC+vXHNAp5pt4QbFVKA=";
+          };
+          postInstall = ''
+            mkdir -p completions
 
-          # Generate completions for shell.
-          for shell in bash zsh fish; do
-            cargo xtask completions --out-dir completions $shell
-          done
+            # Generate completions for shell.
+            for shell in bash zsh fish; do
+              cargo xtask completions --out-dir completions $shell
+            done
 
-          # Install the generated files.
-          installShellCompletion completions/*
+            # Install the generated files.
+            installShellCompletion completions/*
 
-          # Generate and install man pages.
-          cargo xtask man --out-dir gen
-          installManPage gen/nh.1
-        '';
-        env.NH_REV = version;
-      });
+            # Generate and install man pages.
+            cargo xtask man --out-dir gen
+            installManPage gen/nh.1
+          '';
+          env.NH_REV = version;
+        });
+      };
       enable = true;
       clean = {
         enable = true;
