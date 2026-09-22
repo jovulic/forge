a67209a99eeefbfa245af94dd761cda1aecf3bb7
Josip Vulic <jovulic@gmail.com>
2026-09-22 18:42:47 +1000

    add(tools): add util-linux package

--- modules/system/tools.nix
+++ modules/system/tools.nix
@@ -20,6 +20,7 @@ with lib;
   };
   config = mkIf cfg.enable {
     environment.systemPackages = [
+      pkgs.util-linux # set of system utilities for linux
       pkgs.bc # commandline arithmetic
       pkgs.dig # better dns tool than nslookup
       pkgs.mesa-demos # Collection of demos and test programs for OpenGL and Mesa (glxinfo)
